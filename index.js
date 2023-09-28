c682fef62be709b56db58b616872caa8920aec1b
Lemein <lemeinannette@gmail.com>
2023-09-28 22:09:44 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Fridah Jacobs",
+    streetAddress: "186 street"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Update the 'employee' object directly
+    employee[key] = value;
+    return employee; 
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key from the 'employee' object directly
+    delete employee[key];
+    return employee; 
+}
